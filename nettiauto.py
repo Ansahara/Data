3c717f0aa2c30319c3bd6f124aeee42039cbbd88
Ansahara <arttu.ansaharju@hotmail.com>
2018-03-27 00:03:03 +0300

    autojenhaku_kaikiltasivuilta_valmis

--- nettiauto.py
+++ nettiauto.py
@@ -29,39 +29,27 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 #haetaan seuraavan sivun url
 url = soup.find('a', class_ = 'pageNavigation next_link').get('href')
+url = 'https://www.nettiauto.com' + url
 
-linkit = []
+#luodaan lista, johon lisätään ansimäisen ja toisen sivun url
+linkit = ['https://www.nettiauto.com/audi/a3?page=1']
 linkit.append(url)
 
 def rekursio(url, linkit):
-    url = 'https://www.nettiauto.com' + url
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    tarkistus = soup.find('span', class_ = 'disable next_link')
-    print(type(tarkistus))
-    if type(tarkistus) == None:
-        return linkit
+    tarkistus = soup.find_all('span', class_ = 'disable next_link')
+    if len(tarkistus) == 1:
+        return
     else:
         url = soup.find('a', class_ = 'pageNavigation next_link').get('href')
+        url = 'https://www.nettiauto.com' + url
         linkit.append(url)
         return rekursio(url, linkit)
     
 rekursio(url, linkit)
 
-
-###for linkki in linkit:
-
-
-
-#haetaan kaikki autoihin liittyvät linkit
-car_containers = soup.find_all('a', class_ = 'childVifUrl tricky_link')
-data_box = soup.find_all('div', class_ = 'data_box')
-infos = soup.find_all('div', class_ = 'vehicle_other_info clearfix_nett')
-
-
-#haetaan seuraavan sivun url
-url = soup.find('a', class_ = 'pageNavigation next_link').get('href')
 
 
 #luodaan listoja ominaisuuksille
@@ -75,38 +63,55 @@ ids = []
 moottori = []
 malli = []
 
-#käydään infoboksi läpi ja lisätään tiedot listoihin
-for info in infos:
-    lis = info.find_all('li')
-    i = 1
-    for li in lis:
-        jakojaannos = i % 4
-        if jakojaannos == 1:
-            vuosimalli.append(int(li.text))
-        elif jakojaannos == 2:
-            mittarilukema.append(li.text)
-        elif jakojaannos == 3:
-            kayttovoima.append(li.text)
-        elif jakojaannos == 0:
-            vaihteisto.append(li.text)
-        i += 1
+
+for linkki in linkit:
+    response = requests.get(linkki)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+
+
+    #haetaan kaikki autoihin liittyvät linkit
+    car_containers = soup.find_all('a', class_ = 'childVifUrl tricky_link')
+    data_box = soup.find_all('div', class_ = 'data_box')
+    infos = soup.find_all('div', class_ = 'vehicle_other_info clearfix_nett')
+
+
+    #käydään infoboksi läpi ja lisätään tiedot listoihin
+    for info in infos:
+        lis = info.find_all('li', class_ = 'bold')
+        vuosimalli.append(lis[0].text)
+        if len(lis) == 2:   
+            mittarilukema.append(lis[1].text)
+        else:
+            mittarilukema.append('0')
+    
+    
+        lis = info.find_all('li')
+        if len(lis) == 4:
+            kayttovoima.append(lis[2].text)
+            vaihteisto.append(lis[3].text)
+        else:
+            kayttovoima.append('99999')
+            vaihteisto.append('99999')
+            
         
             
-#käydään läpi data_boxi
-for data in data_box:
-    moottori.append(data.span.text)
-    malli.append(data.div.text)
+    #käydään läpi data_boxi
+    for data in data_box:
+        moottori.append(data.span.text)
+        malli.append(data.div.text)
 
-#käydään auto kerraallaan läpi  linkki ja lisätään tiedot listaan
-for car in car_containers:
-    links.append(car.get('href'))
-    hinta.append(int(car.get('data-price')))
-    ids.append(car.get('data-id'))
+    #käydään auto kerraallaan läpi  linkki ja lisätään tiedot listaan
+    for car in car_containers:
+        links.append(car.get('href'))
+        hinta.append(car.get('data-price'))
+        ids.append(car.get('data-id'))
     
     
 
     
-"""
+"""autokohtaiselta sivulta haettava tieto
+tulee aika paljon http pyyntöjä.....
 
 #pyydetään auton sivua
 response = requests.get('https://www.nettiauto.com/skoda/octavia/9349082')
@@ -173,8 +178,13 @@ for i in range(len(lista)):
 df = pd.DataFrame({'hinta':hinta, 'mittarilukema':mittarilukema, 
                    'vuosimalli':vuosimalli, 'kayttovoima':kayttovoima,
                    'moottori':moottori, 'vaihteisto':vaihteisto})
+    
+#muutetaan hinta numeroksi
+df['hinta'] = pd.to_numeric(df['hinta'])
+df['hinta'] = df['hinta'].fillna(0)
 
 #muutetaan mittarilukema numeroksi
+df['mittarilukema'] = df['mittarilukema'].str.replace('Ajamaton','0')
 df['mittarilukema'] = df['mittarilukema'].str.replace('km','')
 df['mittarilukema'] = df['mittarilukema'].str.replace(' ','')
 df['mittarilukema'] = pd.to_numeric(df['mittarilukema'])
@@ -191,6 +201,8 @@ kayttovoima_osuudet = df['kayttovoima'].value_counts().to_frame('maara')
 #muutetaan kayttovoima numeroksi
 df['kayttovoima'] = df['kayttovoima'].str.replace('Bensiini', '0')
 df['kayttovoima'] = df['kayttovoima'].str.replace('Diesel', '1')
+df['kayttovoima'] = df['kayttovoima'].str.replace('Hybridi', '2')
+df['kayttovoima'] = df['kayttovoima'].str.replace('Kaasu', '3')
 df['kayttovoima'] = pd.to_numeric(df['kayttovoima'])
 
 #muutetaan vaihteisto numeroksi
@@ -198,7 +210,7 @@ df['vaihteisto'] = df['vaihteisto'].str.replace('Manuaali', '0')
 df['vaihteisto'] = df['vaihteisto'].str.replace('Automaatti', '1')
 df['vaihteisto'] = pd.to_numeric(df['vaihteisto'])
 
-"""
+
 #Datan muovaamista ja visualisointia
 
 #luodaan uusi datafreimi, jossa muut arvot vuosimallin mukaan keskiarvona
@@ -225,7 +237,9 @@ plt.show()
 
 kayttovoima_osuudet['maara'].plot.pie()
 plt.show()
-"""
+
+
+
 
 #aloitetaan koneoppimisosuus
 
